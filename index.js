7e22376ccaa4b797e804ac731687e643fad8267c
verah <verahnyagaka101@gmail.com>
2022-06-13 12:45:46 +0300

    commit tree 0ce345cc3f6eadc13dc2f5ce63a552e08153d673

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+
+const input = document.getElementById('input');
 function addingEventListener() {
-    const input = document.getElementById('input');
 input.addEventListener('click', function() {
   alert('I was clicked!');
 });
 }
 addingEventListener();
+
+//OR 
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
